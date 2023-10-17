cfd0f02f25e03ee432bbb8e81b5f8953cddc72cf
desu <>
2023-10-17 17:05:53 +0200

    Remove UPX and use OptimizationPreference

--- build.cmd
+++ build.cmd
@@ -4,6 +4,4 @@ cd vencord-plugins
 cd ..\src
 move /y ..\vencord-plugins\a.zip files
 dotnet publish -r win-x64 -c Release /p:DebugType=none /p:DebugSymbols=false
-cd bin\Release\net8.0\win-x64\publish
-upx -9 venkurt.exe
 pause
